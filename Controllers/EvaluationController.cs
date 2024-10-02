7be490f0a4e6f02658efabc70b324641a8f0dc85
joshrayos <rayos@line.com.ph>
2024-10-02 12:15:52 +0800

    evaluation module 10-02-24

--- Controllers/EvaluationController.cs
+++ Controllers/EvaluationController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PartsIq.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,27 @@ namespace PartsIq.Controllers
 {
     public class EvaluationController : Controller
     {
+        private PartsIQEntities db = new PartsIQEntities();
+        private IDataEntityContext dbContext;
+
+        public EvaluationController()
+        {
+            dbContext = new DataEntityContext();
+        }
+
         // GET: Evaluation
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: /Evaluation/GetEvaluationData
+        public JsonResult GetEvaluationData()
+        {
+            var res = dbContext.CreatePendingEvaluations();
+            var data = dbContext.GetEvaluationData();
+
+            return Json(new { data }, JsonRequestBehavior.AllowGet);
         }
     }
 }
