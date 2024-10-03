b4d5812e9b7de06bdd34ebaaf3018cce4c0d0ab6
joshrayos <rayos@line.com.ph>
2024-10-03 15:07:49 +0800

    Evaluation details

--- Controllers/EvaluationController.cs
+++ Controllers/EvaluationController.cs
@@ -23,13 +23,50 @@ namespace PartsIq.Controllers
             return View();
         }
 
+        // GET: /Evaluation/Details/id
+        public ActionResult Details(int id)
+        {
+            var detail = dbContext.GetEvaluationDataById(id);
+            if (detail == null)
+            {
+                return HttpNotFound();
+            }
+            var decisions = DecisionListItem();
+            decisions.RemoveAt(0);
+            var defaultUserID = 5; // Change this to Session ID
+            var defaultUser = dbContext.GetUserDataByID(defaultUserID);
+            if (detail.EvaluatorID.HasValue == false)
+            {
+                detail.EvaluatorName = defaultUser.Name;
+                detail.EvaluatorID = defaultUser.UserId;
+            } 
+            
+            ViewBag.DecisionList = decisions;
+            return View("Details", detail);
+        }
+
         // GET: /Evaluation/GetEvaluationData
         public JsonResult GetEvaluationData()
         {
             var res = dbContext.CreatePendingEvaluations();
-            var data = dbContext.GetEvaluationData();
+            var data = dbContext.GetEvaluationsData();
 
             return Json(new { data }, JsonRequestBehavior.AllowGet);
         }
+
+        #region HELPERS
+        /// <summary>
+        /// Convert Decisions from the database into a list of SelectListItem
+        /// </summary>
+        /// <returns>returns a List(SelectListItem) of SelectListItem from Decision</returns>
+        public List<SelectListItem> DecisionListItem()
+        {
+            return db.Decisions.Select(d => new SelectListItem
+            {
+                Value = d.DecisionID.ToString(),
+                Text = d.Name.ToString(),
+            }).ToList();
+        }
+        #endregion
     }
 }
