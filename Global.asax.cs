efd1ffe145cb565ab9c5a516104bebd930b0f8e1
joshrayos <rayos@line.com.ph>
2024-10-09 08:03:04 +0800

    Added HTTP Error Handlers

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PartsIq.Controllers;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -16,6 +18,34 @@ namespace PartsIq
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            var exception = Server.GetLastError();
+            var httpException = exception as HttpException;
+
+            if (httpException != null)
+            {
+                var statusCode = httpException.GetHttpCode();
+                Response.Clear();
+
+                switch (statusCode)
+                {
+                    case 400:
+                        Response.Redirect("~/Error/BadRequest");
+                        break;
+                    case 404:
+                        Response.Redirect("~/Error/NotFound");
+                        break;
+                    case 500:
+                        Response.Redirect("~/Error/InternalError");
+                        break;
+                    default:
+                        Response.Redirect("~/Error/General");
+                        break;
+                }
+            }
         }
     }
 }
