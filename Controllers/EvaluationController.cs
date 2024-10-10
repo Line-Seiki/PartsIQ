b2e2e25747f8a31f2c1d4fa851c0e54904e483e2
Dandan Ragos <ragos@line.com.ph>
2024-10-10 09:54:14 +0800

    Evaluation

--- Controllers/EvaluationController.cs
+++ Controllers/EvaluationController.cs
@@ -1,6 +1,7 @@
 ﻿using PartsIq.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,11 +27,22 @@ namespace PartsIq.Controllers
         // GET: /Evaluation/Details/id
         public ActionResult Details(int id)
         {
-            var detail = dbContext.GetEvaluationDataById(id);
-            if (detail == null)
+            var find = db.DeliveryDetails.Find(id);
+            if (find == null)
             {
                 return HttpNotFound();
             }
+            var detail = dbContext.GetEvaluationDataById(id);
+
+            if (detail == null) 
+            {
+                return HttpNotFound();
+            }
+            if (detail.DecisionID != 1 )
+            {
+                return Json("Already evaluated or not yet inspected", JsonRequestBehavior.AllowGet);
+            }
+           
             var decisions = DecisionListItem();
             decisions.RemoveAt(0);
             var defaultUserID = 5; // Change this to Session ID
@@ -40,7 +52,15 @@ namespace PartsIq.Controllers
                 detail.EvaluatorName = defaultUser.Name;
                 detail.EvaluatorID = defaultUser.UserId;
             } 
+
+            var part = db.Parts.Find(detail.PartID);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            var checkpoints  = part.Checkpoints.ToList();
             
+            ViewBag.Checkpoints = checkpoints;
             ViewBag.DecisionList = decisions;
             return View("Details", detail);
         }
@@ -48,10 +68,75 @@ namespace PartsIq.Controllers
         // GET: /Evaluation/GetEvaluationData
         public JsonResult GetEvaluationData()
         {
-            var res = dbContext.CreatePendingEvaluations();
-            var data = dbContext.GetEvaluationsData();
+            var delivery = db.DeliveryDetails.Include(drs => drs.Delivery)
+            .Include(dr => dr.Inspection) // Eagerly load related Inspection
+            .Select(s => new EvaluationData
+            {
+                
+                DeliveryDetailID = s.DeliveryDetailID,
+                DecisionID = s.DecisionID,
+                DecisionName = s.Decision.Name, // Accessing related Decision entity
+                EvaluatorID = s.Inspection.EvaluatorID, // Accessing related Inspection fields
+                EvaluatorName = s.Inspection.User.FirstName + " " + s.Inspection.User.LastName,
+                //InspectorID = s.Inspection.UserID,
+                //Inspector = s.Inspection.User.FirstName + " " + s.Inspection.User.LastName,
+                ControlNumber = s.Inspection.ControlNumber,
+                InspectorComments = s.Inspection.InspectionComments,
+                Comments = s.Inspection.Comments,
+                DateFinished = s.Inspection.DateEnd,
+                PartCode = s.Delivery.Part.Code,
+                PartName = s.Delivery.Part.Name,
+                LotNumber = s.LotNumber,
+                LotQuantity = s.LotQuantity,
+                Time = s.Inspection.InspectionDuration.HasValue ? s.Inspection.InspectionDuration.Value : 0,
+                Purpose = "" // Placeholder for dynamic value
+            }).Where(s => s.DecisionID == 1).ToList();
+            return Json(new { message = "success", data = delivery.ToList() }, JsonRequestBehavior.AllowGet);
 
-            return Json(new { data }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateEvaluation()
+        {
+            try
+            {
+                var form = Request.Form;
+
+                if (form == null)
+                {
+                    return HttpNotFound();
+                }
+                var detailID = form.Get("DeliveryDetailID");
+                var decision = form.Get("Decision");
+                var evaluator = form.Get("Evaluator");
+                var inspector = 1;
+                var comment = form.Get("Comments");
+
+                var deliveryDetail = db.DeliveryDetails.Find(Convert.ToInt32(detailID));
+                if (deliveryDetail == null)
+                {
+                    return HttpNotFound();
+                }
+                deliveryDetail.DecisionID = Convert.ToInt32(decision);
+                var inspection = db.Inspections.Find(deliveryDetail.InspectionID);
+                if (inspection == null)
+                {
+                    return HttpNotFound();
+                }
+                inspection.EvaluatorID = Convert.ToInt32(evaluator);
+                inspection.InspectionComments = comment;
+
+                db.SaveChanges();
+
+                return Json(new { message = "Success", success = true }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new { message = $"Error ${ex.Message} occured.", success = false }, JsonRequestBehavior.AllowGet);
+            }
+
         }
 
         #region HELPERS
