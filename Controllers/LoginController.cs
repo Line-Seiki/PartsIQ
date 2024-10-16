430dc21e774ee323a7ed2c9f8f8e80468cd2d10b
Dandan Ragos <ragos@line.com.ph>
2024-10-16 09:39:58 +0800

    Login draft with salt

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PartsIq.Models;
+using PartsIq.Utility;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,28 @@ namespace PartsIq.Controllers
 {
     public class LoginController : Controller
     {
+        private PartsIQEntities db = new PartsIQEntities();
         // GET: Login
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login (string username, string password)
+        {
+            var user = db.Users.FirstOrDefault(u => u.Username == username);
+            if (user == null) 
+            {
+                return Json(new { success = false, message = $"Username or password is not valid." }, JsonRequestBehavior.AllowGet);
+            }
+            bool isPasswordValid = UserHelper.VerifyPassword(password, user.Password, user.Salt);
+            if (isPasswordValid)
+            {
+
+            }
+            return Json(new { success = true, message = "Test" }, JsonRequestBehavior.AllowGet);
+
         }
     }
 }
